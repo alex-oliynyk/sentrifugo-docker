392e6a22703afee6352b1f192b0508b493ae095e
Alex Olink <soliynyk01@gmail.com>
2021-08-19 23:35:22 +0300

    add new dump

---
+++ app/sentrifugo/public/db_constants.php
@@ -0,0 +1,6 @@
+<?php
+	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','db');
+	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','sentrifugo');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','howtoforge');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','sentrifugodb');         
+?>
